5aafc51e18480f5d05a3c30ab0b76a712fc05cec
unknown <izzy.zammil@gmail.com>
2022-05-19 21:24:22 +0700

    Slicing Navbar and Intro Left Side

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Intro.css";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import Boy from "../../img/boy.png";
+import ThumbUp from "../../img/thumbup.png";
+import Crown from "../../img/crown.png";
+import GlassesImoji from "../../img/glassesimoji.png";
+import FloatingDiv from "../FloatingDiv/FloatingDiv";
+
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am</span>
+          <span>Izzy Zammil</span>
+          <span>
+            Beginner Front End Developer using React Js and Mobile Developer
+            using React Native
+          </span>
+        </div>
+        <button className="button i-button">Hire me</button>
+        <div className="i-icons">
+          <img src={Github} alt="" />
+          <img src={LinkedIn} alt="" />
+          <img src={Instagram} alt="" />
+        </div>
+      </div>
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={Boy} alt="" />
+
+        <div>
+          <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
