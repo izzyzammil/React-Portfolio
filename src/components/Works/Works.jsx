764c1aaa6d6eba2545d6cc588deb37c159b06f4c
unknown <izzy.zammil@gmail.com>
2022-05-20 17:01:30 +0700

    Slicing Work Section

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./Works.css";
+import UpWork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
 
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <div className="works">
+      <div className="awesome">
+        <span>Work for All these</span>
+        <span>Brand & Client</span>
+        <span>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
+          at?
+          <br />
+          Lorem ipsum dolor sit amet.
+          <br />
+          Lorem ipsum dolor sit amet consectetur.
+          <br />
+          Lorem ipsum dolor sit.
+        </span>
+        <button className="button s-button">Hire me</button>
+
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+
+      <div className="w-right">
+        <div className="w-mainCircle">
+          <div className="w-secCircle">
+            <img src={UpWork} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiverr} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" />
+          </div>
+        </div>
+
+        <div className="w-backCircle blueCircle"></div>
+        <div className="w-backCircle yellowCircle"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Works;
